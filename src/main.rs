1d7dfce43d25a4395e420cfaa2b34ddc2c9259cb
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2024-04-12 09:54:25 -0700

    Read the file in

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let Some(filename) = env::args()
@@ -14,6 +14,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Ok(());
     };
 
+    if !filename.exists() {
+        eprintln!("File {} does not exist", filename.display());
+        return Ok(());
+    }
+
     println!("Reading from {}", filename.display());
+
+    let raw_input = fs::read_to_string(filename)?;
+    println!("{}", raw_input);
+
     Ok(())
 }
